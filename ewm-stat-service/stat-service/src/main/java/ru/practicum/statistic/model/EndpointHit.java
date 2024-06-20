2f40f3c3bf3d58c0999d861f364776b48ddc4775
koryruno <koryruno@yandex.ru>
2024-06-20 16:36:59 +0300

    v.0.0.1-SNAPSHOT

--- ewm-stat-service/stat-service/src/main/java/ru/practicum/statistic/model/EndpointHit.java
+++ ewm-stat-service/stat-service/src/main/java/ru/practicum/statistic/model/EndpointHit.java
@@ -7,13 +7,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
