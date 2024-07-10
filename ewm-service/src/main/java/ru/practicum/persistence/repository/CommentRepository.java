7d372326ca0e47e212a4f0d5f5ac378204e1e39f
koryruno <koryruno@yandex.ru>
2024-07-11 00:38:11 +0300

    dev: feature-comments 0.1.0-SS

--- ewm-service/src/main/java/ru/practicum/persistence/repository/CommentRepository.java
+++ ewm-service/src/main/java/ru/practicum/persistence/repository/CommentRepository.java
@@ -12,16 +12,4 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findAllByEventId(Long eventId, PageRequest page);
 
-//    @Query("SELECT c FROM Comment c " +
-//            "WHERE (:text IS NULL OR c.text LIKE %:text%) " +
-//            "AND (:events IS NULL OR c.event.id IN :events) " +
-//            "AND (:rangeStart IS NULL OR c.created >= :rangeStart) " +
-//            "AND (:rangeEnd IS NULL OR c.created <= :rangeEnd) " +
-//            "AND (:status IS NULL OR c.status = :status)")
-//    List<Comment> searchComments(@Param("text") String text,
-//                                 @Param("events") List<Long> events,
-//                                 @Param("rangeStart") LocalDateTime rangeStart,
-//                                 @Param("rangeEnd") LocalDateTime rangeEnd,
-//                                 @Param("status") CommentStatus status,
-//                                 PageRequest page);
 }
