3f9201cc0f1e95098166e08c1fe2b0f4beb2feaf
koryruno <koryruno@yandex.ru>
2024-07-12 18:54:23 +0300

    fix: feature-comments

--- ewm-service/src/main/java/ru/practicum/common/mapper/CommentMapper.java
+++ ewm-service/src/main/java/ru/practicum/common/mapper/CommentMapper.java
@@ -4,8 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.practicum.api.requestDto.NewCommentDto;
 import ru.practicum.api.responseDto.CommentDto;
+import ru.practicum.common.enums.CommentStatus;
 import ru.practicum.persistence.model.Comment;
+import ru.practicum.persistence.model.Event;
+import ru.practicum.persistence.model.User;
 
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,8 +21,12 @@ public class CommentMapper {
     private final EventMapper eventMapper;
     private final UserMapper userMapper;
 
-    public Comment toComment(NewCommentDto newCommentDto) {
+    public Comment toNewComment(User user, Event event, NewCommentDto newCommentDto) {
         return Comment.builder()
+                .event(event)
+                .author(user)
+                .created(LocalDateTime.now())
+                .status(CommentStatus.PENDING)
                 .text(newCommentDto.getText())
                 .build();
     }
@@ -38,6 +46,18 @@ public class CommentMapper {
         return comments.stream()
                 .map(this::toCommentDto)
                 .collect(Collectors.toList());
+    }
+
+    public void adminUpdateCommentFromDto(Comment comment, NewCommentDto newCommentDto) {
+        comment.setText(newCommentDto.getText());
+        comment.setCreated(LocalDateTime.now());
+        comment.setStatus(CommentStatus.APPROVED);
+    }
+
+    public void privateUpdateCommentFromDto(Comment comment, NewCommentDto newCommentDto) {
+        comment.setText(newCommentDto.getText());
+        comment.setCreated(LocalDateTime.now());
+        comment.setStatus(CommentStatus.PENDING);
     }
 
 }
